01df3980c829c622825d34ee63b7b29abf0407f1
Olaf Kirch <okir@suse.de>
2022-10-17 16:40:57 +0200

    oracle: allow EFI variable events to be rebuilt using tpm_parsed_event_rebuild()
    Signed-off-by: Olaf Kirch <okir@suse.de>

--- src/eventlog.c
+++ src/eventlog.c
@@ -481,12 +481,54 @@ __tpm_event_efi_variable_print(tpm_parsed_event_t *parsed, tpm_event_bit_printer
 }
 
 static bool
+__tpm_event_marshal_efi_variable(bufbuilder_t *bp, const tpm_parsed_event_t *parsed, const void *raw_data, unsigned int raw_data_len)
+{
+	unsigned int var_len, name_len;
+
+	if (!bufbuilder_put(bp, parsed->efi_variable_event.variable_guid, sizeof(parsed->efi_variable_event.variable_guid)))
+		return false;
+
+	var_len = strlen(parsed->efi_variable_event.variable_name);
+	if (!bufbuilder_put_u64le(bp, var_len)
+	 || !bufbuilder_put_u64le(bp, raw_data_len)
+	 || !bufbuilder_put_utf16le(bp, parsed->efi_variable_event.variable_name, &name_len)
+	 || !bufbuilder_put(bp, raw_data, raw_data_len))
+		return false;
+
+	if (name_len != 2 * var_len)
+		return false;
+
+	return true;
+}
+
+static bufbuilder_t *
+__tpm_event_efi_variable_rebuild(const tpm_parsed_event_t *parsed, const void *raw_data, unsigned int raw_data_len)
+{
+	bufbuilder_t *bp;
+
+	/* The marshal buffer needs to hold
+	 * GUID, 2 * UINT64, plus the UTF16 encoding of the variable name, plus the raw efivar value */
+	bp = bufbuilder_alloc(16 + 8 + 8 +
+			+ 2 * strlen(parsed->efi_variable_event.variable_name)
+			+ raw_data_len);
+
+	if (!__tpm_event_marshal_efi_variable(bp, parsed, raw_data, raw_data_len)) {
+		debug("Failed to marshal EFI variable %s\n", parsed->efi_variable_event.variable_name);
+		bufbuilder_free(bp);
+		return NULL;
+	}
+
+	return bp;
+}
+
+static bool
 __tpm_event_parse_efi_variable(tpm_event_t *ev, tpm_parsed_event_t *parsed, bufparser_t *bp)
 {
 	uint64_t name_len, data_len;
 
 	parsed->destroy = __tpm_event_efi_variable_destroy;
 	parsed->print = __tpm_event_efi_variable_print;
+	parsed->rebuild = __tpm_event_efi_variable_rebuild;
 
 	if (!bufparser_get(bp, parsed->efi_variable_event.variable_guid, sizeof(parsed->efi_variable_event.variable_guid)))
 		return false;
